0de1b96c12ff46dc660e34c30b992fbd0a2454b0
Nuthelk <nuthelkcd@gmail.com>
2022-08-03 13:56:55 -0500

    Login y register sin terminar

--- src/routers/AppRoutes.jsx
+++ src/routers/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Introduce from "../components/Introduce";
 import Login from "../components/Login";
+import Register from "../components/Register";
 
 function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ function AppRoutes() {
         <Route path="/" element={<Introduce text={"felipe"} goTo="/home" />} />
         <Route path="/home" element={<Introduce text={"nuthelk"} goTo="/" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
