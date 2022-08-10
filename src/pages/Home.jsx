74dcac39fe35180f254ccecc52419c02e1637ffc
Nuthelk <nuthelkcd@gmail.com>
2022-08-10 13:46:45 -0500

    sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,32 +7,36 @@ import HomeSwitch from "../components/HomeSwitch/HomeSwitch";
 import { Context } from "../context/ContextProvider";
 import FormAddWorkout from "../components/FormAddWorkout";
 import Button from "../components/Button";
+import SideBar from "../components/SideBar";
 
 function Home() {
-  const { isSelected, handleModal, showModal } = useContext(Context);
+  const { isSelected, handleModal, showModal, showSider, handleSider } = useContext(Context);
 
   return (
-    <>
-      <HomeHeader />
-      <SliderHome />
-      <HomeSwitch />
-      <Categories />
-      <div className="flex flex-col w-full max-w-xl gap-4 mx-auto mt-11">
-        {isSelected ? (
-          <div className="text-center">
-            <Button
-              text={"Create new workout"}
-              type="button"
-              event={handleModal}
-            />
-          </div>
-        ) : (
-          <HomeCard />
-        )}
+    <div className="relative">
+      <div onClick={showSider ? handleSider : null} className={showSider ? "blur-md h-screen md:blur-none  " : "h-screen"}>
+        <HomeHeader />
+        <SliderHome />
+        <HomeSwitch />
+        <Categories />
+        <div className="flex flex-col w-full max-w-xl gap-4 mx-auto mt-11">
+          {isSelected ? (
+            <div className="text-center">
+              <Button
+                text={"Create new workout"}
+                type="button"
+                event={handleModal}
+              />
+            </div>
+          ) : (
+            <HomeCard />
+          )}
+        </div>
       </div>
 
       {showModal && <FormAddWorkout />}
-    </>
+      {showSider && <SideBar />}
+    </div>
   );
 }
 
